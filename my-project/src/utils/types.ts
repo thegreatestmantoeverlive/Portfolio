6849c66cd10fdcfa1a19f6e78638b6f3fde1f1ac
thegreatestmantoeverlive <dsa238998@gmail.com>
2024-08-24 05:23:25 +0000

    added types

--- my-project/src/utils/types.ts
+++ my-project/src/utils/types.ts
@@ -1,39 +1,39 @@
 interface ThumbnailObject {
-    // Define properties for ThumbnailObject here
-    // Example properties:
-    url: string;
-    width: number;
-    height: number;
-  }
-  
-  interface Video {
-    type: "video"; // Constant value of "video"
-  
-    title: string;
-    videoId: string;
-  
-    author: string;
-    authorId: string;
-    authorUrl: string;
-    authorVerified: boolean;
-  
-    videoThumbnails: ThumbnailObject[];
-  
-    description: string;
-    descriptionHtml: string;
-  
-    viewCount: number; // Integer
-    viewCountText: string;
-    lengthSeconds: number; // Integer
-  
-    published: number; // Unix timestamp
-    publishedText: string;
-  
-    // Only available on premiered videos
-    premiereTimestamp?: number; // Unix timestamp (optional)
-  
-    liveNow: boolean;
-    premium: boolean;
-    isUpcoming: boolean;
-  }
-  
+  // Define properties for ThumbnailObject here
+  // Example properties:
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface Video {
+  type: "video"; // Constant value of "video"
+
+  title: string;
+  videoId: string;
+
+  author: string;
+  authorId: string;
+  authorUrl: string;
+  authorVerified: boolean;
+
+  videoThumbnails: ThumbnailObject[];
+
+  description: string;
+  descriptionHtml: string;
+
+  viewCount: number; // Integer
+  viewCountText: string;
+  lengthSeconds: number; // Integer
+
+  published: number; // Unix timestamp
+  publishedText: string;
+
+  // Only available on premiered videos
+  premiereTimestamp?: number; // Unix timestamp (optional)
+
+  liveNow: boolean;
+  premium: boolean;
+  isUpcoming: boolean;
+}
+
